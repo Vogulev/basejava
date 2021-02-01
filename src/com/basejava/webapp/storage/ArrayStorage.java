c464d8b621fb9d70aedb6a5889373f925fca5e57
Vogulev <art.vogulev@gmail.com>
2021-02-01 22:17:36 +0300

    added AbstractArrayStorage and SortedArrayStorage classes

--- src/com/basejava/webapp/storage/ArrayStorage.java
+++ src/com/basejava/webapp/storage/ArrayStorage.java
@@ -7,12 +7,7 @@ import java.util.Arrays;
 /**
  * Array based storage for Resumes
  */
-public class ArrayStorage implements Storage{
-
-    private static final int STORAGE_LIMIT = 10_000;
-
-    private final Resume[] storage = new Resume[STORAGE_LIMIT];
-    private int size = 0;
+public class ArrayStorage extends AbstractArrayStorage implements Storage {
 
     public void clear() {
         Arrays.fill(storage, 0, size, null);
@@ -26,14 +21,6 @@ public class ArrayStorage implements Storage{
             storage[size] = resume;
             size++;
         } else System.out.println("ERROR! Resume with ID " + resume.getUuid() + " is already exist!");
-    }
-
-    public Resume get(String uuid) {
-        int index = getIndex(uuid);
-        if (index != -1) {
-            return storage[index];
-        } else System.out.println("ERROR! Resume with ID " + uuid + " not exist!");
-        return null;
     }
 
     public void delete(String uuid) {
@@ -50,10 +37,6 @@ public class ArrayStorage implements Storage{
         return allResumes;
     }
 
-    public int size() {
-        return size;
-    }
-
     public void update(Resume resume) {
         int index = getIndex(resume.getUuid());
         if (index != -1) {
@@ -61,7 +44,7 @@ public class ArrayStorage implements Storage{
         } else System.out.println("No resume with ID " + resume.getUuid() + " for update!");
     }
 
-    private int getIndex(String uuid) {
+    protected int getIndex(String uuid) {
         for (int i = 0; i < size; i++) {
             if (storage[i].getUuid().equals(uuid)) {
                 return i;
