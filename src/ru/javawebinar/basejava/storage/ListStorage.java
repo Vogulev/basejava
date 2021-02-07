befe24d8f1b3901a8a8ddc0d30406ba9cf971607
Vogulev <art.vogulev@gmail.com>
2021-02-07 15:46:23 +0300

    -Fix getAll() method in ListStorageTest class

--- src/ru/javawebinar/basejava/storage/ListStorage.java
+++ src/ru/javawebinar/basejava/storage/ListStorage.java
@@ -53,7 +53,7 @@ public class ListStorage extends AbstractStorage {
 
     @Override
     public Resume[] getAll() {
-        return (Resume[]) storage.toArray();
+        return storage.toArray(new Resume[0]);
     }
 
     @Override
