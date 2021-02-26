df3d1d2c172a6e2357e1961eb8935e93b37113f9
Artem Vogulev <art.vogulev@gmail.com>
2021-02-26 23:28:28 +0300

    Created ContactType & ResumeTestData classes, Added new functionality to Resume class

--- src/ru/javawebinar/basejava/model/Resume.java
+++ src/ru/javawebinar/basejava/model/Resume.java
@@ -1,16 +1,18 @@
 package ru.javawebinar.basejava.model;
 
-import java.util.Objects;
-import java.util.UUID;
+import java.util.*;
 
 /**
  * Initial resume class
  */
 public class Resume implements Comparable<Resume> {
 
-    // Unique identifier
     private final String uuid;
     private final String fullName;
+    private final Map<ContactType, String> contacts = new EnumMap<>(ContactType.class);
+    private final Map<SectionType, AbstractSection> sections = new EnumMap<>(SectionType.class);
+    private Organization experience;
+    private Organization education;
 
     public Resume(String fullName) {
         this(UUID.randomUUID().toString(), fullName);
@@ -25,6 +27,45 @@ public class Resume implements Comparable<Resume> {
 
     public String getUuid() {
         return uuid;
+    }
+
+    public void printContacts() {
+        for (Map.Entry<ContactType, String> pair : contacts.entrySet()) {
+            System.out.println(pair.getKey().getTitle() + " " + pair.getValue());
+        }
+    }
+
+    public void printSections() {
+        for (Map.Entry<SectionType, AbstractSection> pair : sections.entrySet()) {
+            System.out.println(pair.getKey().getTitle());
+            pair.getValue().getContent();
+        }
+    }
+
+    public void addContact(ContactType contactType, String contact) {
+        contacts.put(contactType, contact);
+    }
+
+    public void addTextSection(SectionType sectionType, String textArea) {
+        sections.put(sectionType, new TextSection(textArea));
+    }
+
+    public void addListSection(SectionType sectionType, List<String> content) {
+        sections.put(sectionType, new ListSection(content));
+    }
+
+    public void addExperienceSection(SectionType sectionType, String companyName, String date, String position, String description) {
+        if (experience == null) {
+            experience = new Organization(companyName, date, position, description);
+            sections.put(sectionType, experience);
+        } else experience.experience.add(new Organization.Experience(companyName, date, position, description));
+    }
+
+    public void addEducationSection(SectionType sectionType, String companyName, String date, String position, String description) {
+        if (education == null) {
+            education = new Organization(companyName, date, position, description);
+            sections.put(sectionType, education);
+        } else education.experience.add(new Organization.Experience(companyName, date, position, description));
     }
 
     @Override
@@ -51,5 +92,66 @@ public class Resume implements Comparable<Resume> {
     public int compareTo(Resume o) {
         int result = fullName.compareTo(o.fullName);
         return result == 0 ? uuid.compareTo(o.uuid) : result;
+    }
+
+    static abstract class AbstractSection {
+        public abstract void getContent();
+    }
+
+    class TextSection extends AbstractSection {
+        private final String textArea;
+
+        public TextSection(String textArea) {
+            this.textArea = textArea;
+        }
+
+        public void getContent() {
+            System.out.println(textArea);
+        }
+    }
+
+    class ListSection extends AbstractSection {
+        private final List<String> content;
+
+        public ListSection(List<String> content) {
+            this.content = content;
+        }
+
+        @Override
+        public void getContent() {
+            for (String text : content) {
+                System.out.println("- " + text);
+            }
+        }
+
+    }
+
+    static class Organization extends AbstractSection {
+        private final List<Experience> experience = new ArrayList<>();
+
+        public Organization(String companyName, String date, String position, String description) {
+            experience.add(new Experience(companyName, date, position, description));
+        }
+
+        @Override
+        public void getContent() {
+            for (Experience exp : experience) {
+                System.out.println(exp.companyName + "\n" + exp.date + " | " + exp.position + "\n" + exp.description);
+            }
+        }
+
+        static class Experience {
+            private final String companyName;
+            private final String date;
+            private final String position;
+            private final String description;
+
+            public Experience(String companyName, String date, String position, String description) {
+                this.companyName = companyName;
+                this.date = date;
+                this.position = position;
+                this.description = description;
+            }
+        }
     }
 }
---
+++ test/ru/javawebinar/basejava/storage/ResumeTestData.java
@@ -0,0 +1,76 @@
+package ru.javawebinar.basejava.storage;
+
+import ru.javawebinar.basejava.model.ContactType;
+import ru.javawebinar.basejava.model.Resume;
+import ru.javawebinar.basejava.model.SectionType;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ResumeTestData {
+
+    public static void main(String[] args) {
+        Resume resume = new Resume("Григорий Кислин");
+        //Fill resume contact fields
+        resume.addContact(ContactType.TELEPHONE, "+7(921) 855-0482");
+        resume.addContact(ContactType.SKYPE, "grigory.kislin");
+        resume.addContact(ContactType.MAIL, "gkislin@yandex.ru");
+        resume.addContact(ContactType.LINKEDIN, "https://www.linkedin.com/in/gkislin/");
+        resume.addContact(ContactType.GITHUB, "https://github.com/gkislin");
+        resume.addContact(ContactType.STACKOVERFLOW, "https://stackoverflow.com/users/548473/grigory-kislin");
+        resume.addContact(ContactType.HOMEPAGE, "http://gkislin.ru/");
+        //Fill resume section fields
+        resume.addTextSection(SectionType.OBJECTIVE, "Ведущий стажировок и корпоративного обучения по Java Web и Enterprise технологиям");
+        resume.addTextSection(SectionType.PERSONAL, "Аналитический склад ума, сильная логика, креативность, инициативность. Пурист кода и архитектуры.");
+        resume.addListSection(SectionType.ACHIEVEMENT, new ArrayList<>(Arrays.asList(
+                "С 2013 года: разработка проектов \"Разработка Web приложения\",\"Java Enterprise\", \"Многомодульный maven. Многопоточность. XML (JAXB/StAX). Веб сервисы (JAX-RS/SOAP). Удаленное взаимодействие (JMS/AKKA)\". Организация онлайн стажировок и ведение проектов. Более 1000 выпускников.",
+                "Реализация двухфакторной аутентификации для онлайн платформы управления проектами Wrike. Интеграция с Twilio, DuoSecurity, Google Authenticator, Jira, Zendesk.",
+                "Налаживание процесса разработки и непрерывной интеграции ERP системы River BPM. Интеграция с 1С, Bonita BPM, CMIS, LDAP. Разработка приложения управления окружением на стеке: Scala/Play/Anorm/JQuery. Разработка SSO аутентификации и авторизации различных ERP модулей, интеграция CIFS/SMB java сервера.",
+                "Реализация c нуля Rich Internet Application приложения на стеке технологий JPA, Spring, Spring-MVC, GWT, ExtGWT (GXT), Commet, HTML5, Highstock для алгоритмического трейдинга.",
+                "Создание JavaEE фреймворка для отказоустойчивого взаимодействия слабо-связанных сервисов (SOA-base архитектура, JAX-WS, JMS, AS Glassfish). Сбор статистики сервисов и информации о состоянии через систему мониторинга Nagios. Реализация онлайн клиента для администрирования и мониторинга системы по JMX (Jython/ Django).",
+                "Реализация протоколов по приему платежей всех основных платежных системы России (Cyberplat, Eport, Chronopay, Сбербанк), Белоруcсии(Erip, Osmp) и Никарагуа.")));
+        resume.addListSection(SectionType.QUALIFICATIONS, new ArrayList<>(Arrays.asList("JEE AS: GlassFish (v2.1, v3), OC4J, JBoss, Tomcat, Jetty, WebLogic, WSO2",
+                "Version control: Subversion, Git, Mercury, ClearCase, Perforce",
+                "DB: PostgreSQL(наследование, pgplsql, PL/Python), Redis (Jedis), H2, Oracle",
+                "MySQL, SQLite, MS SQL, HSQLDB",
+                "Languages: Java, Scala, Python/Jython/PL-Python, JavaScript, Groovy,",
+                "XML/XSD/XSLT, SQL, C/C++, Unix shell scripts,",
+                "Java Frameworks: Java 8 (Time API, Streams), Guava, Java Executor, MyBatis, Spring (MVC, Security, Data, Clouds, Boot), JPA (Hibernate, EclipseLink), Guice, GWT(SmartGWT, ExtGWT/GXT), Vaadin, Jasperreports, Apache Commons, Eclipse SWT, JUnit, Selenium (htmlelements).",
+                "Python: Django.",
+                "JavaScript: jQuery, ExtJS, Bootstrap.js, underscore.js",
+                "Scala: SBT, Play2, Specs2, Anorm, Spray, Akka",
+                "Технологии: Servlet, JSP/JSTL, JAX-WS, REST, EJB, RMI, JMS, JavaMail, JAXB, StAX, SAX, DOM, XSLT, MDB, JMX, JDBC, JPA, JNDI, JAAS, SOAP, AJAX, Commet, HTML5, ESB, CMIS, BPMN2, LDAP, OAuth1, OAuth2, JWT.",
+                "Инструменты: Maven + plugin development, Gradle, настройка Ngnix,",
+                "администрирование Hudson/Jenkins, Ant + custom task, SoapUI, JPublisher, Flyway, Nagios, iReport, OpenCmis, Bonita, pgBouncer.",
+                "Отличное знание и опыт применения концепций ООП, SOA, шаблонов проектрирования, архитектурных шаблонов, UML, функционального программирования",
+                "Родной русский, английский \"upper intermediate\"")));
+        resume.addExperienceSection(SectionType.EXPERIENCE, "http://javaops.ru/", "10/2013 - Сейчас", "Автор проекта.",
+                "Создание, организация и проведение Java онлайн проектов и стажировок.");
+        resume.addExperienceSection(SectionType.EXPERIENCE, "https://www.wrike.com/", "10/2014 - 01/2016", "Старший разработчик (backend)",
+                "Проектирование и разработка онлайн платформы управления проектами Wrike (Java 8 API, Maven, Spring, MyBatis, Guava, Vaadin, PostgreSQL, Redis). Двухфакторная аутентификация, авторизация по OAuth1, OAuth2, JWT SSO.");
+        resume.addExperienceSection(SectionType.EXPERIENCE, "RIT Center", "04/2012 - 10/2014", "Java архитектор",
+                "Организация процесса разработки системы ERP для разных окружений: релизная политика, версионирование, ведение CI (Jenkins), миграция базы (кастомизация Flyway), конфигурирование системы (pgBoucer, Nginx), AAA via SSO. Архитектура БД и серверной части системы. Разработка интергационных сервисов: CMIS, BPMN2, 1C (WebServices), сервисов общего назначения (почта, экспорт в pdf, doc, html). Интеграция Alfresco JLAN для online редактирование из браузера документов MS Office. Maven + plugin development, Ant, Apache Commons, Spring security, Spring MVC, Tomcat,WSO2, xcmis, OpenCmis, Bonita, Python scripting, Unix shell remote scripting via ssh tunnels, PL/Python");
+        resume.addExperienceSection(SectionType.EXPERIENCE, "http://www.luxoft.ru/", "12/2010 - 04/2012", "Ведущий программист",
+                "Участие в проекте Deutsche Bank CRM (WebLogic, Hibernate, Spring, Spring MVC, SmartGWT, GWT, Jasper, Oracle). Реализация клиентской и серверной части CRM. Реализация RIA-приложения для администрирования, мониторинга и анализа результатов в области алгоритмического трейдинга. JPA, Spring, Spring-MVC, GWT, ExtGWT (GXT), Highstock, Commet, HTML5.");
+        resume.addExperienceSection(SectionType.EXPERIENCE, "https://www.yota.ru/", "06/2008 - 12/2010", "Ведущий специалист",
+                "Дизайн и имплементация Java EE фреймворка для отдела \"Платежные Системы\" (GlassFish v2.1, v3, OC4J, EJB3, JAX-WS RI 2.1, Servlet 2.4, JSP, JMX, JMS, Maven2). Реализация администрирования, статистики и мониторинга фреймворка. Разработка online JMX клиента (Python/ Jython, Django, ExtJS)");
+        resume.addExperienceSection(SectionType.EXPERIENCE, "http://enkata.com/", "03/2007 - 06/2008", "Разработчик ПО",
+                "Реализация клиентской (Eclipse RCP) и серверной (JBoss 4.2, Hibernate 3.0, Tomcat, JMS) частей кластерного J2EE приложения (OLAP, Data mining).");
+        resume.addExperienceSection(SectionType.EXPERIENCE, "https://www.siemens.com/ru/ru/home.html", "01/2005 - 02/2007", "Разработчик ПО",
+                "Разработка информационной модели, проектирование интерфейсов, реализация и отладка ПО на мобильной IN платформе Siemens @vantage (Java, Unix).");
+        resume.addExperienceSection(SectionType.EXPERIENCE, "http://www.alcatel.ru/", "09/1997 - 01/2005", "Инженер по аппаратному и программному тестированию",
+                "Тестирование, отладка, внедрение ПО цифровой телефонной станции Alcatel 1000 S12 (CHILL, ASM).");
+        resume.addEducationSection(SectionType.EDUCATION, "https://www.coursera.org/course/progfun", "03/2013 - 05/2013", "\"Functional Programming Principles in Scala\" by Martin Odersky", null);
+        resume.addEducationSection(SectionType.EDUCATION, "http://www.luxoft-training.ru/training/catalog/course.html?ID=22366", "03/2011 - 04/2011", "Курс \"Объектно-ориентированный анализ ИС. Концептуальное моделирование на UML.\"", null);
+        resume.addEducationSection(SectionType.EDUCATION, "http://www.siemens.ru/", "01/2005 - 04/2005", "3 месяца обучения мобильным IN сетям (Берлин)", "");
+        resume.addEducationSection(SectionType.EDUCATION, "http://www.alcatel.ru/", "09/1997 - 03/1998", "6 месяцев обучения цифровым телефонным сетям (Москва)", "");
+        resume.addEducationSection(SectionType.EDUCATION, "http://www.ifmo.ru/", "09/1993 - 07/1996", "Аспирантура (программист С, С++)", "");
+        resume.addEducationSection(SectionType.EDUCATION, "", "09/1987 - 07/1993", "Инженер (программист Fortran, C)", "");
+        resume.addEducationSection(SectionType.EDUCATION, "http://www.school.mipt.ru/", "09/1984 - 06/1987", "Закончил с отличием", "");
+
+        resume.printContacts();
+        System.out.println();
+        resume.printSections();
+
+    }
+}
