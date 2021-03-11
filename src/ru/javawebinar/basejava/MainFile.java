4b67a31c141b78f4fb16575062407f9e048a70ea
Artem Vogulev <art.vogulev@gmail.com>
2021-03-11 23:04:46 +0300

    Added spaces functionality to MainFile class

--- src/ru/javawebinar/basejava/MainFile.java
+++ src/ru/javawebinar/basejava/MainFile.java
@@ -5,19 +5,19 @@ import java.io.File;
 public class MainFile {
     public static void main(String[] args) {
         MainFile mainFile = new MainFile();
-        mainFile.walkFileTree(new File("./src/ru/javawebinar/basejava"));
+        mainFile.walkFileTree(new File("./src"), "");
     }
 
-    public void walkFileTree(File dir) {
+    public void walkFileTree(File dir, String spaces) {
         File[] list = dir.listFiles();
         if (list != null) {
             for (File file : list) {
                 if (file.isFile()) {
-                    System.out.println("File: " + file.getName());
+                    System.out.println(spaces + "File: " + file.getName());
                 }
                 if (file.isDirectory()) {
-                    System.out.println("Directory: " + file.getName());
-                    walkFileTree(file);
+                    System.out.println(spaces + "Directory: " + file.getName());
+                    walkFileTree(file, spaces.concat(" "));
                 }
             }
         }
