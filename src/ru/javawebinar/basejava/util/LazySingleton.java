3340bc50982afb95fa88def52929d59ab23e285d
Artem Vogulev <art.vogulev@gmail.com>
2021-03-26 16:28:41 +0300

    -Singleton, DeadLockTest, LazySingleton classes created

---
+++ src/ru/javawebinar/basejava/util/LazySingleton.java
@@ -0,0 +1,19 @@
+package ru.javawebinar.basejava.util;
+
+public class LazySingleton {
+    private static LazySingleton INSTANCE;
+
+    private LazySingleton() {
+    }
+
+    public static LazySingleton getInstance() {
+        if (INSTANCE == null) {
+            synchronized (LazySingleton.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = new LazySingleton();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+}
